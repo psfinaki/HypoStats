5e9466f3ad073391c6fc2fb7519a40bf43d328de
Petr <psfinaki@users.noreply.github.com>
2025-10-14 23:00:48 +0200

    Add Room schema (#8)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
     id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+    id("androidx.room") version "2.6.1" apply false
 }
