e8a4f63c3cfca8ff54280eea7f053b9192190f40
Petr <psfinaki@users.noreply.github.com>
2025-10-24 18:23:29 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "app.hypostats"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "0.4.2-dev"
+        versionCode = 7
+        versionName = "0.4.3-dev"
     }
     
     room {
