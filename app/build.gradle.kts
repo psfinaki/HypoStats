3197e1042a13f09bdb5e0ad5b1196f75ea6eb47a
Petr <psfinaki@users.noreply.github.com>
2025-11-09 13:31:30 +0100

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "app.hypostats"
         minSdk = 26
         targetSdk = 36
-        versionCode = 50
-        versionName = "0.5.0"
+        versionCode = 51
+        versionName = "0.5.1"
         testInstrumentationRunner = "app.hypostats.HiltTestRunner"
     }
 
