b30c25425002b60f8ec524ed2fc0aeacf89b34d0
Petr <psfinaki@users.noreply.github.com>
2025-11-26 18:01:23 +0100

    Add more information to the About screen (#38)

---
+++ app/src/main/kotlin/app/hypostats/ui/about/AboutLinkItem.kt
@@ -0,0 +1,54 @@
+package app.hypostats.ui.about
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import app.hypostats.R
+
+@Composable
+fun AboutLinkItem(
+    text: String,
+    onClick: () -> Unit,
+) {
+    Row(
+        modifier =
+            Modifier
+                .fillMaxWidth()
+                .clickable(onClick = onClick)
+                .padding(vertical = 16.dp, horizontal = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.bodyLarge,
+        )
+        Icon(
+            painter = painterResource(R.drawable.open_in_new_24),
+            contentDescription = stringResource(R.string.about_open_link),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun AboutLinkItemPreview() {
+    MaterialTheme {
+        AboutLinkItem(
+            text = "Source Code",
+            onClick = {},
+        )
+    }
+}
